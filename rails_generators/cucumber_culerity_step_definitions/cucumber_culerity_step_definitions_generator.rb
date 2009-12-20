069de4615a9426be3a60138019a27d63597d1273
Jason Gladish <jason@expectedbehavior.com>
2009-12-20 13:50:31 -0500

    pluginizing with generator

---
+++ rails_generators/cucumber_culerity_step_definitions/cucumber_culerity_step_definitions_generator.rb
@@ -0,0 +1,17 @@
+class CucumberCulerityStepDefinitionsGenerator < Rails::Generator::Base
+  
+  def manifest
+    record do |m|
+      m.directory 'features/step_definitions'
+      m.file 'features/step_definitions/common_celerity_steps.rb', 'features/step_definitions/common_celerity_steps.rb'
+      m.file 'features/step_definitions/model_create_steps.rb', 'features/step_definitions/model_create_steps.rb'
+    end
+  end
+
+protected
+
+  def banner
+    "Usage: #{$0} cucumber_culerity_step_definitions"
+  end
+
+end
