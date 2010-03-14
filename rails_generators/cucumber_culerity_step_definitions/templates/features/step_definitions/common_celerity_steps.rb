1ebac07955b21106db05b71d3935870040512082
Jason Gladish <jason@expectedbehavior.com>
2010-03-13 20:17:10 -0500

    pulling in some updates from upstream that might improve performance, and fixing issue with killing the rails server after a run

--- rails_generators/cucumber_culerity_step_definitions/templates/features/step_definitions/common_celerity_steps.rb
+++ rails_generators/cucumber_culerity_step_definitions/templates/features/step_definitions/common_celerity_steps.rb
@@ -1,21 +1,28 @@
 require 'culerity'
 
 Before do
-  $rails_server ||= Culerity::run_rails(:environment => 'culerity', :port => 3001)
+  $rails_server_pid ||= Culerity::run_rails(:environment => 'culerity', :port => 3001)
   $server ||= Culerity::run_server
-  $browser = Culerity::RemoteBrowserProxy.new $server, {:browser => :firefox3,
-    :javascript_exceptions => true,
-    :resynchronize => true,
-    :status_code_exceptions => true
-  }
-  $browser.log_level = :warning
+  unless $browser
+    $browser = Culerity::RemoteBrowserProxy.new $server, {:browser => :firefox3,
+      :javascript_exceptions => true,
+      :resynchronize => true,
+      :status_code_exceptions => true
+    }
+    $browser.log_level = :warning
+  end
   @host = 'http://localhost:3001'
+end
+
+After do
+  $server.clear_proxies
+  $browser.clear_cookies
 end
 
 at_exit do
   $browser.exit if $browser
   $server.close if $server
-  Process.kill(6, $rails_server.pid.to_i) if $rails_server
+  Process.kill(6, $rails_server_pid) if $rails_server_pid
 end
 
 Given /^I am at the "(.*)" host$/ do |hostname|
